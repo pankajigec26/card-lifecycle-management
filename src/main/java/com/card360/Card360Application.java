9219f2f819494e6b11bcd8974a20d8ce7c69c207
pankajigec26 <pankajigec26@gmail.com>
2026-09-19 22:43:18 +0530

    Add Card360 application source

---
+++ src/main/java/com/card360/Card360Application.java
@@ -0,0 +1,9 @@
+package com.card360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Card360Application {
+  public static void main(String[] args) { SpringApplication.run(Card360Application.class, args); }
+}
